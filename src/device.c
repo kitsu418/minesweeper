97b02f0caf252971b118650e26c4cc4928a6ea63
Weiyi Chen <wychen325@gmail.com>
2023-10-24 15:04:32 +0800

    feat: add linker script to build riscv executable

--- src/device.c
+++ src/device.c
@@ -1,5 +1,16 @@
 #include "device.h"
 
+extern unsigned __stacktop;
+// initial stack pointer is first address of program
+__attribute__((section(".stack"), used)) unsigned* __stack_init = &__stacktop;
+
+extern int main();
+
+__attribute__((section(".text.start"))) __attribute__((naked)) void _start() {
+  asm("mv sp, %0\n\t" ::"r"(&__stacktop));
+  asm("j %0\n\t" ::"i"(&main));
+}
+
 void set_vram(int x, int y, uint8_t color) {
   VRAM_DATA_ADDR[x * VRAM_WIDTH + y] = color;
 }
